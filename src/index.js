e3727976f71a62c2617a5e84d17f6999977d9c67
Harshit Jain <hjain.tech@gmail.com>
2019-06-25 21:44:52 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -3,8 +3,26 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { MuiThemeProvider } from '@material-ui/core/styles';
+import { createMuiTheme } from '@material-ui/core/styles';
+import blue from '@material-ui/core/colors/lightBlue';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const theme = () => {
+    return(createMuiTheme({
+      palette: {
+        primary: {
+          ...blue,
+          '500': '#01579B'
+        }
+      }
+    }));
+  }
+
+ReactDOM.render(
+    <MuiThemeProvider>
+        <App />
+    </MuiThemeProvider>
+, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ src/pages/Landing/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './LandingCss';
+import Button from '@material-ui/core/Button';
+
+const Landing = () => {
+  return (
+    <Button variant="contained" color="primary">
+        Primary 123
+      </Button>
+  );
+}
+
+export default Landing;
