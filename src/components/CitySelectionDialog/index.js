3840ae96625c703da7af33125ccd64c28305f448
Harshit Jain <hjain.tech@gmail.com>
2019-06-30 20:48:43 +0530

    Code cleanup

--- src/components/CitySelectionDialog/index.js
+++ src/components/CitySelectionDialog/index.js
@@ -4,42 +4,6 @@ import DialogTitle from '@material-ui/core/DialogTitle';
 import Dialog from '@material-ui/core/Dialog';
 import styles from './CitySelectionDialogCss';
 
-const _cityList = [{
-    name: 'Bangalore',
-    url: 'https://images.oyoroomscdn.com/uploads/city_image/small/shutterstock_bangalore.jpg'
-}, {
-    name: 'Gurgaon',
-    url: 'https://images.oyoroomscdn.com/uploads/city_image/small/gurgaon city.jpg'
-}, {
-    name: 'Pune',
-    url: 'https://images.oyoroomscdn.com/uploads/city_image/small/shutterstock_pune.jpg'
-}, {
-    name: 'Noida',
-    url: 'https://images.oyoroomscdn.com/uploads/city_image/small/noida.jpg'
-}, {
-    name: 'Mumbai',
-    url: 'https://images.oyoroomscdn.com/uploads/city_image/small/mumbai.jpg'
-}, {
-    name: 'Delhi',
-    url: 'https://images.oyoroomscdn.com/uploads/city_image/small/delhi.jpg'
-}, {
-    name: 'Hyderabad',
-    url: 'https://images.oyoroomscdn.com/uploads/city_image/small/hyderabad.jpg'
-}, {
-    name: 'Chennai',
-    url: ' https://images.oyoroomscdn.com/uploads/city_image/small/chennai.jpg'
-}, {
-    name: 'Kolkata',
-    url: 'https://images.oyoroomscdn.com/uploads/city_image/small/kolkata.jpg'
-}, {
-    name: 'Ghaziabad',
-    url: 'https://images.oyoroomscdn.com/uploads/city_image/small/ghaziabad_new.jpg'
-}, {
-    name: 'Greater Noida',
-    url: 'https://images.oyoroomscdn.com/uploads/city_image/small/greater_noida_new.jpg'
-}
-];
-
 const getCityListJSX = (cityList) => {
     return cityList.map(city => (
         <CityCard
@@ -49,6 +13,7 @@ const getCityListJSX = (cityList) => {
     ));
 }
 const CitySelectionDialog = () => {
+    // State to maintain city list
     const [cityList, setCityList] = React.useState([]);
 
     React.useEffect(() => {
@@ -58,6 +23,7 @@ const CitySelectionDialog = () => {
             setCityList(data);
           });
       }, []);
+
     return (
         <Dialog onClose={() => { }} open>
             <DialogTitle>{'Please select a city'}</DialogTitle>
