e0f30d84a70c6bb00aeaffc2c1453bfcdd895fb4
Harshit Jain <hjain.tech@gmail.com>
2019-06-30 20:48:10 +0530

    Stiched dummy API and used react hooks

--- src/components/CitySelectionDialog/index.js
+++ src/components/CitySelectionDialog/index.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import CityCard from '../CityCard';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import Dialog from '@material-ui/core/Dialog';
@@ -7,61 +7,67 @@ import styles from './CitySelectionDialogCss';
 const _cityList = [{
     name: 'Bangalore',
     url: 'https://images.oyoroomscdn.com/uploads/city_image/small/shutterstock_bangalore.jpg'
-  },{
+}, {
     name: 'Gurgaon',
     url: 'https://images.oyoroomscdn.com/uploads/city_image/small/gurgaon city.jpg'
-  },{
+}, {
     name: 'Pune',
     url: 'https://images.oyoroomscdn.com/uploads/city_image/small/shutterstock_pune.jpg'
-  },{
+}, {
     name: 'Noida',
     url: 'https://images.oyoroomscdn.com/uploads/city_image/small/noida.jpg'
-  },{
+}, {
     name: 'Mumbai',
     url: 'https://images.oyoroomscdn.com/uploads/city_image/small/mumbai.jpg'
-  },{
+}, {
     name: 'Delhi',
     url: 'https://images.oyoroomscdn.com/uploads/city_image/small/delhi.jpg'
-  },{
+}, {
     name: 'Hyderabad',
     url: 'https://images.oyoroomscdn.com/uploads/city_image/small/hyderabad.jpg'
-  },{
+}, {
     name: 'Chennai',
     url: ' https://images.oyoroomscdn.com/uploads/city_image/small/chennai.jpg'
-  },{
+}, {
     name: 'Kolkata',
     url: 'https://images.oyoroomscdn.com/uploads/city_image/small/kolkata.jpg'
-  },{
+}, {
     name: 'Ghaziabad',
     url: 'https://images.oyoroomscdn.com/uploads/city_image/small/ghaziabad_new.jpg'
-  },{
+}, {
     name: 'Greater Noida',
     url: 'https://images.oyoroomscdn.com/uploads/city_image/small/greater_noida_new.jpg'
-  }
-  ];
+}
+];
 
-class CitySelectionDialog extends Component {
-    
-    
-    render() {
-        return (
-          <Dialog onClose={() => { }} open>
-            <DialogTitle>Select City</DialogTitle>
+const getCityListJSX = (cityList) => {
+    return cityList.map(city => (
+        <CityCard
+            name={city.name}
+            url={city.url}
+        />
+    ));
+}
+const CitySelectionDialog = () => {
+    const [cityList, setCityList] = React.useState([]);
+
+    React.useEffect(() => {
+        fetch('https://my-json-server.typicode.com/hjaintech/mockAPI/cities')
+          .then(results => results.json())
+          .then(data => {
+            setCityList(data);
+          });
+      }, []);
+    return (
+        <Dialog onClose={() => { }} open>
+            <DialogTitle>{'Please select a city'}</DialogTitle>
             <div style={styles.cityList}>
-              {this.getCityList()}
+                {getCityListJSX(cityList)}
             </div>
-          </Dialog>
-        );
-      }
-      
-      getCityList = () => {
-        return _cityList.map(city => (
-          <CityCard
-              name={city.name}
-              url={city.url}
-            />
-        ));
-      }
+        </Dialog>
+    );
+
+
 }
 
 export default CitySelectionDialog;
